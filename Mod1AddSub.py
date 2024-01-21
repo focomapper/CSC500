e8f12c2d21c4d5aec68a81fc97ae1ac5b2e549a6
Jim Chappell <117658412+focomapper@users.noreply.github.com>
2024-01-21 10:39:13 -0800

    Add files via upload

---
+++ Mod1AddSub.py
@@ -0,0 +1,15 @@
+# Get user input as float to handle decimals
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+
+# Add input
+result = number1 + number2
+
+# Print result
+print(number1, "plus", number2, "=", result)
+
+# Subtract input
+result = number1 - number2
+
+# Print result
+print(number1, "minus", number2, "=", result)
---
+++ Mod1MultDiv.py
@@ -0,0 +1,15 @@
+# Get user input as float to handle decimals
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+
+# Multiply input
+result = number1 * number2
+
+# Print result
+print(number1, "times", number2, "=", result)
+
+# Divide the numbers
+result = number1 / number2
+
+# Print result
+print(number1, "divided by", number2, "=", result)
